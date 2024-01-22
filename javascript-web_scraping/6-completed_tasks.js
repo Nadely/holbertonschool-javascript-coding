a9bc3726f141f3d4d54ca048044de67c32d75674
Nadely <nadege.tette@holbertonstudents.com>
2024-01-22 17:07:02 +0100

    add code in task 6

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Failed to fetch data. Status Code:', response.statusCode);
+    return;
+  }
+
+  const todos = JSON.parse(body);
+  const completedTasks = {};
+
+  todos.forEach((task) => {
+    if (task.completed) {
+      const { userId } = task;
+
+      if (!completedTasks[userId]) {
+        completedTasks[userId] = 1;
+      } else {
+        completedTasks[userId] += 1;
+      }
+    }
+  });
+
+  const result = {};
+  Object.entries(completedTasks).forEach(([userId, completedTasks]) => {
+    result[userId] = completedTasks;
+  });
+
+  console.log(result);
+});
