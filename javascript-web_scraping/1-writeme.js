b948d70e9e956f47577b8b3b3d47ed28b9a3bd55
Nadely <nadege.tette@holbertonstudents.com>
2024-01-22 11:16:19 +0100

    add file for Write a script that writes a string to a file.

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// Write a script that writes a string to a file.
+
+const fs = require('fs')
+
+const filePath = process.argv[2];
+const string = process.argv[3];
+
+fs.writeFile(filePath, string, 'utf-8', (err, fileContent) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  } else {
+    console.log(fileContent);
+  }
+});
