5212d8cc668b587f9d0417e3089730ef734e3472
Nadely <nadege.tette@holbertonstudents.com>
2024-01-22 17:20:56 +0100

    cheker test

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
-// Write a script that display the status code of a GET request.
-
-const request = require('request');
+const fetch = require('node-fetch');
 
 const url = process.argv[2];
 
-request.get(url, (error, response) => {
-  if (error) {
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    console.log('code:', response.status);
+  })
+  .catch(error => {
     console.error(`Error: ${error.message}`);
-  } else {
-    console.log('code:', response.statusCode);
-  }
-});
+  });
