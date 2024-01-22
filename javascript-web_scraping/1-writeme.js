19d49ab6819174804e5ea6133973132ed361d188
Nadely <nadege.tette@holbertonstudents.com>
2024-01-22 11:51:04 +0100

    correction style in task 1 and add file for Write a script that display the status code of a GET request.

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // Write a script that writes a string to a file.
 
-const fs = require('fs')
+const fs = require('fs');
 
 const filePath = process.argv[2];
 const string = process.argv[3];
 
-fs.writeFile(filePath, string, 'utf-8', (err, fileContent) => {
+fs.writeFile(filePath, string, 'utf-8', (err) => {
   if (err) {
     console.error(err);
     process.exit(1);
