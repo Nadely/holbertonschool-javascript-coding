8b3e96e89837e62ef1bab0dadb01c3b30ea3f439
Nadely <nadege.tette@holbertonstudents.com>
2024-01-22 11:58:03 +0100

    Update task 2 to delete one condition

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -6,9 +6,5 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, { method: 'GET' }, (error, response) => {
-  if (error) {
-    console.error('Error:', error);
-  } else {
-    console.log('code:', response.statusCode);
-  }
+  console.log('code:', response.statusCode);
 });
