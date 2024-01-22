5303ce4f67a32905ab37cfecfc2ecf46e9d0c43b
Nadely <nadege.tette@holbertonstudents.com>
2024-01-22 11:02:08 +0100

    add file for Write a script that reads and prints the content of a file.

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,11 +2,13 @@
 // Write a script that reads and prints the content of a file.
 
 const fs = require('fs');
+
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', (err, fileContent) => {
   if (err) {
     console.error(err);
+    process.exit(1);
   } else {
     console.log(fileContent);
   }
