acb83b753c3da87a158434156098e40fd2bdaaa2
Nadely <nadege.tette@holbertonstudents.com>
2024-01-22 14:19:00 +0100

    update task 5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -5,17 +5,16 @@ const request = require('request');
 const fs = require('fs');
 
 const url = process.argv[2];
-const file_path = process.argv[3];
+const filePath = process.argv[3];
 
 request.get(url, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-        return;
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
     }
-
-    fs.writeFile(file_path, body, 'utf-8', (err) => {
-        if (err) {
-            console.error('Error writing to file:', err);
-		}
-    });
+  });
 });
