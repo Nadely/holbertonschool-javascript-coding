8ec6ad2d7fa239a3f2e51dbc9ce328a5c10d2bef
Nadely <nadege.tette@holbertonstudents.com>
2024-01-22 17:21:50 +0100

    remove tasks 2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
-const fetch = require('node-fetch');
+// Write a script that display the status code of a GET request.
+
+const request = require('request');
 
 const url = process.argv[2];
 
-fetch(url)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    console.log('code:', response.status);
-  })
-  .catch(error => {
+request.get(url, (error, response) => {
+  if (error) {
     console.error(`Error: ${error.message}`);
-  });
+  } else {
+    console.log('code:', response.statusCode);
+  }
+});
