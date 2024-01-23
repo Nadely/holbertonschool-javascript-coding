a4408ebf401d0e4a1f67cfdb4e37ed05f9712f5b
Nadely <nadege.tette@holbertonstudents.com>
2024-01-23 21:24:08 +0100

    remove task 6

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,19 +1,17 @@
 #!/usr/bin/node
 
-const util = require('util');
 const request = require('request');
-const chalk = require('chalk');
 
 const url = process.argv[2];
 
 request.get(url, (error, response, body) => {
   if (error) {
-    console.error(chalk.red('Error:'), error);
+    console.error('Error:', error);
     return;
   }
 
   if (response.statusCode !== 200) {
-    console.error(chalk.red('Failed to fetch data. Status Code:'), response.statusCode);
+    console.error('Failed to fetch data. Status Code:', response.statusCode);
     return;
   }
 
@@ -32,9 +30,5 @@ request.get(url, (error, response, body) => {
     }
   });
 
-  const formattedOutput = util.inspect(completedTasks, { depth: null, colors: true })
-    .replace(/^\{\s*/, '{')
-    .replace(/\s*\}\s*$/, '}');
-
-  console.log(formattedOutput);
+  console.log(completedTasks);
 });
