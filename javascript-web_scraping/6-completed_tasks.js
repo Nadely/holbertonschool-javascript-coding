c2b194909983527052afccbd9aa8685015263bac
Nadely <nadege.tette@holbertonstudents.com>
2024-01-24 00:36:56 +0100

    update again task 6

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -8,28 +8,22 @@ const url = process.argv[2];
 request.get(url, (error, response, body) => {
   if (error) {
     console.error('Error:', error);
-    return;
-  }
+  } else {
+    const todos = JSON.parse(body);
+    const completedTasks = {};
 
-  if (response.statusCode !== 200) {
-    console.error('Failed to fetch data. Status Code:', response.statusCode);
-    return;
-  }
+    todos.forEach((task) => {
+      if (task.completed) {
+        const { userId } = task;
 
-  const todos = JSON.parse(body);
-  const completedTasks = {};
-
-  todos.forEach((task) => {
-    if (task.completed) {
-      const { userId } = task;
-
-      if (!completedTasks[userId]) {
-        completedTasks[userId] = 1;
-      } else {
-        completedTasks[userId] += 1;
+        if (!completedTasks[userId]) {
+          completedTasks[userId] = 1;
+        } else {
+          completedTasks[userId] += 1;
+        }
       }
-    }
-  });
+    });
 
-  console.log(completedTasks);
+    console.log(completedTasks);
+  }
 });
