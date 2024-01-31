5f1ea6b5c790acb0d678b9c71d708a52422b18ca
Nadely <nadege.tette@holbertonstudents.com>
2024-01-31 16:38:10 +0100

    update task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 function welcome() {
-  process.stdout.write('Welcome to Holberton School, what is your name?');
+  process.stdout.write('Welcome to Holberton School, what is your name?\n');
   process.stdin.setEncoding('utf8');
 
   process.stdin.on('data', (input) => {
     const name = input.toString();
     process.stdout.write(`Your name is: ${name}`);
 
-    process.stdout.write('This important software is now closing');
+    process.stdout.write('This important software is now closing\n');
     process.exit();
   });
 }
