8e7bf8a67d425390e28a1285de1105572bb38c4a
Nadely <nadege.tette@holbertonstudents.com>
2024-01-31 17:47:12 +0100

    add task 6

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+module.exports = app;
+
+if (require.main === module) {
+  app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
+    });
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+}
