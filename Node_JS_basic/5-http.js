bdf4cc72337058a8068f164597ee28639ed656ca
Nadely <nadege.tette@holbertonstudents.com>
2024-01-31 16:52:01 +0100

    update task 5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,54 +1,48 @@
 const http = require('http');
-const fs = require('fs');
+const fs = require('fs').promises;
 const countStudentsAsync = require('./3-read_file_async');
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
   if (req.url === '/') {
     res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
+  }
+  if (req.url === '/students') {
     const database = 'database.csv';
 
-    countStudentsAsync(database)
-      .then(() => {
-        fs.readFile(database, 'utf8', (err, data) => {
-          if (err) {
-            res.end('Cannot load the database');
-            return;
-          }
+    try {
+      await countStudentsAsync(database);
+      const data = await fs.readFile(database, 'utf8');
+      const lines = data.trim().split('\n').slice(1);
+      const studentsByField = {};
 
-          const lines = data.trim().split('\n').slice(1);
-          const studentsByField = {};
-
-          lines.forEach((line) => {
-            const [firstname, lastname, age, field] = line.split(',');
-            if (field && field.trim() !== '') {
-              if (!studentsByField[field]) {
-                studentsByField[field] = [];
-              }
-              studentsByField[field].push(firstname);
-            }
-          });
-
-          let responseText = 'This is the list of our students\n';
-          let totalStudents = lines.length;
-
-          responseText += `Number of students: ${totalStudents}\n`;
-
-          for (const field in studentsByField) {
-            if (Object.prototype.hasOwnProperty.call(studentsByField, field)) {
-              const students = studentsByField[field];
-              responseText += `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}\n`;
-            }
-          }
-
-          res.end(responseText);
-        });
-      })
-      .catch(() => {
-        res.end('Cannot load the database');
+      lines.forEach((line) => {
+        // eslint-disable-next-line no-unused-vars
+        const [firstname, lastname, age, field] = line.split(',');
+        if (field && field.trim() !== '') {
+          studentsByField[field] = studentsByField[field] || [];
+          studentsByField[field].push(firstname);
+        }
       });
+
+      let responseText = 'This is the list of our students\n';
+      let totalStudents = lines.length;
+
+      responseText += `Number of students: ${totalStudents}\n`;
+
+      for (const field in studentsByField) {
+        if (Object.prototype.hasOwnProperty.call(studentsByField, field)) {
+          const students = studentsByField[field];
+          responseText += `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}\n`;
+        }
+      }
+
+      res.end(responseText);
+    } catch (err) {
+      console.error('Error:', err);
+      res.end('Cannot load the database');
+    }
   }
 });
 
