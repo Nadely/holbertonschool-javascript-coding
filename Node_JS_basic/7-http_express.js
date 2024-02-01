802218da4dafcd9599902e8f2c87dcc32401f397
Nadely <nadege.tette@holbertonstudents.com>
2024-02-01 13:52:03 +0100

    update task 7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -13,12 +13,11 @@ app.get('/students', async (req, res) => {
   const mess1 = 'This is the list of our students';
   countStudentsAsync(database)
     .then((result) => {
-
       const mess2 = result.sentence1;
       const mess3 = result.sentence1;
       const mess4 = result.sentence1;
-      const message = `${mess1}\n${mess2}\n${mess3}\n${mess4}`
-      res.type('text/plain').send(message)
+      const message = `${mess1}\n${mess2}\n${mess3}\n${mess4}`;
+      res.type('text/plain').send(message);
     })
     .catch((error) => {
       const err = `${mess1}\n${error.message}`;
