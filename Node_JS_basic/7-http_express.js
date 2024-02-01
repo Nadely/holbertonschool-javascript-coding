12ca01c3cc1fd7a1699dc2d57079331d979e7e78
Nadely <nadege.tette@holbertonstudents.com>
2024-02-01 13:42:50 +0100

    update task 7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -3,8 +3,6 @@ const countStudentsAsync = require('./3-read_file_async');
 
 const app = express();
 
-
-
 const database = 'database.csv';
 
 app.get('/', (req, res) => {
@@ -13,20 +11,20 @@ app.get('/', (req, res) => {
 
 app.get('/students', async (req, res) => {
   res.write('This is the list of our students\n');
-    countStudentsAsync(database)
-      .then((result) => {
-        res.write(`${result.sentence1}\n`);
-        res.write(`${result.sentence2}\n`);
-        res.write(`${result.sentence3}`);
-        res.end();
-      })
-      .catch((error) => {
-        res.end(error.message);
-      });
-  });
+  countStudentsAsync(database)
+    .then((result) => {
+      res.write(`${result.sentence1}\n`);
+      res.write(`${result.sentence2}\n`);
+      res.write(`${result.sentence3}`);
+      res.end();
+    })
+    .catch((error) => {
+      res.end(error.message);
+    });
+});
 
 app.listen(1245, () => {
-  console.log(`Server is running on http://localhost:1245`);
+  console.log('Server is running on http://localhost:1245');
 });
 
 module.exports = app;
