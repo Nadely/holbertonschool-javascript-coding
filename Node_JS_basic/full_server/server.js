64c3b324275609b8206be1ced7287b9f9d5167f4
Nadely <6890@holbertonstudents.com>
2024-02-02 20:42:29 +0100

    update task 8

--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,14 +1,14 @@
-import routes from'./routes';
+import routes from './routes';
 const express = require('express');
 
-const port = 1245;
 const app = express();
+const port = 1245;
 
 app.use('/', routes);
 
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running at http://localhost:${port}/`);
 });
 
 module.exports = app;
